13a5eaf5f99947c78f2d49f70057004da940ebb4
Nikita Grishaev <grishaev@rightway.games>
2020-12-17 00:44:46 +0300

    Базовая реализация State Machine с тремя стейтами. Базовый GameBoostraper

---
+++ Assets/Scripts/Infrastructure/States/GameLoopState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Infrastructure.States
+{
+    public class GameLoopState : IState
+    {
+        public GameLoopState(StateMachine stateMachine)
+        {
+        }
+
+        public void Exit()
+        {
+        }
+
+        public void Enter()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Infrastructure/States/IState.cs
@@ -0,0 +1,17 @@
+﻿namespace Infrastructure.States
+{
+    public interface IExitableState
+    {
+        void Exit();
+    }
+
+    public interface IState : IExitableState
+    {
+        void Enter();
+    }
+
+    public interface IContextState<TContext> : IExitableState
+    {
+        void Enter(TContext context);
+    }
+}
