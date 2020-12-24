1eb6acbc342116cd1b03a8359eeddd9f241682c4
Nikita Grishaev <grishaev@rightway.games>
2020-12-24 23:38:36 +0300

    Prefab for loading game from any scene

---
+++ Assets/Scripts/Infrastructure/GameRunner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Infrastructure
+{
+    public class GameRunner : MonoBehaviour
+    {
+        [SerializeField] private GameBoostraper _bootstrappersPrefab;
+    
+        private void Awake()
+        {
+            if (FindObjectOfType<GameBoostraper>() == null)
+                Instantiate(_bootstrappersPrefab);
+        }
+    }
+}
