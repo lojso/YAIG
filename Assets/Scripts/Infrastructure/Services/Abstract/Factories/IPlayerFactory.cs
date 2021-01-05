0c8daa34974d83a1624a653e269bb24903c6456f
Nikita Grishaev <grishaev@rightway.games>
2021-01-05 22:52:57 +0300

    Created Player Factory

---
+++ Assets/Scripts/Infrastructure/Services/Abstract/Factories/IPlayerFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GameLogic.Player;
+
+namespace Infrastructure.Services.Abstract.Factories
+{
+    public interface IPlayerFactory : IService
+    {
+        event Action<Player> OnPlayerCreated;
+        
+        Player Player { get; }
+
+        Player CreatePlayer();
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Services/Factories/PlayerFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GameLogic.Player;
+using Infrastructure.Services.Abstract.Factories;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Infrastructure.Services.Factories
+{
+    public class PlayerFactory : IPlayerFactory
+    {
+        public event Action<Player> OnPlayerCreated;
+        public Player Player { get; private set; }
+        
+        public Player CreatePlayer()
+        {            
+            var playerPrefab = Resources.Load<Player>(AssetsPath.Enemy);
+            
+            Player = Object.Instantiate(playerPrefab);
+            
+            OnPlayerCreated?.Invoke(Player);
+            return Player;
+        }
+    }
+}
