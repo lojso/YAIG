2e940eb4da522a74902d90d4ffc5db886628e576
Nikita Grishaev <grishaev@rightway.games>
2021-01-19 09:47:31 +0300

    Animation Clip factory

---
+++ Assets/Scripts/Infrastructure/Services/Abstract/Factories/IAnimationFactory.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Infrastructure.Services.Abstract.Factories
+{
+    public interface IAnimationFactory
+    {
+        Animator CreateAnimationClipPrefab();
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Services/Factories/AnimationFactory.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Services.Abstract.Factories;
+using UnityEngine;
+
+namespace Infrastructure.Services.Factories
+{
+    public class AnimationFactory : IAnimationFactory
+    {
+        public Animator CreateAnimationClipPrefab()
+        {
+            var cameraPrefab = Resources.Load<Animator>(AssetsPath.AnimationClip);
+            return Object.Instantiate(cameraPrefab);       
+        }
+    }
+}
