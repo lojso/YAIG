1648d332da55fc420e824abba9f4506722ba7174
Nikita Grishaev <grishaev@rightway.games>
2021-02-25 00:34:28 +0300

    Extensions
    Took 19 minutes

---
+++ Assets/Scripts/Infrastructure/FluentExtensions/FluentExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Infrastructure.FluentExtensions
+{
+    public static class FluentExtensions
+    {
+        public static T Do<T>(this T obj, Action<T> action)
+        {
+            return Do<T>(obj, action, true);
+        }
+
+        public static T Do<T>(this T obj, Action<T> action, bool when)
+        {
+            if (when) 
+                action?.Invoke(obj);
+
+            return obj;
+        }
+        
+        public static T Do<T>(this T obj, Action<T> action, Func<bool> when)
+        {
+            if (when()) 
+                action?.Invoke(obj);
+
+            return obj;
+        }
+    }
+}
